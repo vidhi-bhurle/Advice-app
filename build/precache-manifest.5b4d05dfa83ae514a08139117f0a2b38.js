cc99130687ae9772552d1032a1ac5c27ddbbfe88
vidhi-bhurle <109790862+vidhi-bhurle@users.noreply.github.com>
2022-07-27 12:31:04 +0530

    Add files via upload

---
+++ build/precache-manifest.5b4d05dfa83ae514a08139117f0a2b38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84274fb506580f69f7e454ef83526eae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbdaffa99a28cf58c7ae",
+    "url": "/static/css/main.6d21c723.chunk.css"
+  },
+  {
+    "revision": "63685f232ee7924a567c",
+    "url": "/static/js/2.f675d9cd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f675d9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbdaffa99a28cf58c7ae",
+    "url": "/static/js/main.6e9450a6.chunk.js"
+  },
+  {
+    "revision": "54cd695355a35b7373f6",
+    "url": "/static/js/runtime-main.4b623690.js"
+  },
+  {
+    "revision": "2637c791de76278f56d8c2e6f58c9c85",
+    "url": "/static/media/v2osk-1Z2niiBPg5A-unsplash.2637c791.jpg"
+  }
+]);
